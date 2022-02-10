bf6b9d46d1df936198f6c8f2f6e1df91803984bf
João Victor S. P <victoronstudy@gmail.com>
2022-02-10 09:55:40 -0300

    Adiciona arquivo de configurações

---
+++ prototipo/config.py
@@ -0,0 +1,21 @@
+"""
+Configurações e valores fixos utilizados no jogo.
+"""
+
+altura_do_bloco = 20
+largura_do_bloco = 20
+
+"""
+Referência dos números de tipo do bloco:
+    "O número do tipo do bloco é igual ao índice do caminho da imagem do
+    bloco na lista."
+
+Por exemplo: 0 = water_1.png
+"""
+
+caminhos_das_imagens_dos_blocos = (
+        "./prototipo/imagens/water_1.png",
+        "./prototipo/imagens/ground_1.png",
+        "./prototipo/imagens/grass_1.png"
+        )
+

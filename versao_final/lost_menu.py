1e9a526e41ef4b448a0638a16b2baec671f83353
João Victor S. P <victoronstudy@gmail.com>
2022-03-18 21:08:16 -0300

    Adiciona telas de ganhou jogo e de perdeu jogo

--- versao_final/lost_menu.py
+++ versao_final/lost_menu.py
@@ -1,9 +1,23 @@
+from os.path import join
 from abc_menu import AbcMenu
 from menu_button import MenuButton
 
 
 class LostMenu(AbcMenu):
+    """
+    Represents the screen for when the player loses the game.
+    """
+    
     def __init__(self):
-        super().__init__([MenuButton('sprites/start.png', 'main', 500, 300),
-                          MenuButton('sprites/exit.png', 'quit', 100, 300)],
-                         'sprites/background_colorido.jpg', 'lost')
+        return_button_image_path = \
+                join('images', 'return_to_menu_button.png')
+        return_to_menu_button = \
+                MenuButton(return_button_image_path, 'main', 334, 520)
+        quit_button_image_path = \
+                join('images', 'quit_button_large.png')
+        quit_button = \
+                MenuButton(quit_button_image_path, 'quit', 595, 670)
+        buttons = [return_to_menu_button, quit_button]
+        background_path = join('images', 'background_failed_screen.png')
+        name = 'lost'
+        super().__init__(buttons, background_path, name)
